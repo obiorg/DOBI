c5a441199a8bb8c5c8049d816f1cbcbc99f57e7d
Hendrick <19176533+kfoxone@users.noreply.github.com>
2025-07-02 23:30:49 +0100

    Correction des prolème d'intégration de persistence'

--- dobi-core/src/main/java/org/dobi/entities/Persistence.java
+++ dobi-core/src/main/java/org/dobi/entities/Persistence.java
@@ -12,11 +12,11 @@ import java.util.Date;
 @Table(name = "persistence")
 public class Persistence extends BaseEntity {
 
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Basic(optional = false)
-//    @Column(name = "id")
-//    private Integer id;
+    //    @Id
+    //    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //    @Basic(optional = false)
+    //    @Column(name = "id")
+    //    private Integer id;
     @Column(name = "deleted")
     private Boolean deleted;
     @Column(name = "created")
@@ -29,11 +29,11 @@ public class Persistence extends BaseEntity {
     private String comment;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "machine", nullable = false)
+    @JoinColumn(name = "company", nullable = false)
     private Company company;
 
     @ManyToOne
-    @JoinColumn(name = "tag_id")
+    @JoinColumn(name = "tag") // <-- CORRECTION ICI : "tag" au lieu de "tag_id"
     private Tag tag;
 
     @Column(name = "method")
@@ -42,30 +42,52 @@ public class Persistence extends BaseEntity {
     @Column(name = "activate")
     private Boolean activate = false;
 
-    public Company getCompany() {return company;}
-    public void setCompany(Company company) {this.company = company;}
-    public Tag getTag() {return tag;}
-    public void setTag(Tag tag) {this.tag = tag;}
-    public Integer getMethod() {return method;}
-    public void setMethod(Integer method) {this.method = method;}
-    public Boolean getActivate() {return activate;}
-    public void setActivate(Boolean activate) {this.activate = activate;}
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public Tag getTag() {
+        return tag;
+    }
+
+    public void setTag(Tag tag) {
+        this.tag = tag;
+    }
+
+    public Integer getMethod() {
+        return method;
+    }
+
+    public void setMethod(Integer method) {
+        this.method = method;
+    }
+
+    public Boolean getActivate() {
+        return activate;
+    }
+
+    public void setActivate(Boolean activate) {
+        this.activate = activate;
+    }
 
     public Persistence() {
     }
 
-//    public Persistence(Integer id) {
-//        this.id = id;
-//    }
-//
-//    public Integer getId() {
-//        return id;
-//    }
-//
-//    public void setId(Integer id) {
-//        this.id = id;
-//    }
-
+    //    public Persistence(Integer id) {
+    //        this.id = id;
+    //    }
+    //
+    //    public Integer getId() {
+    //        return id;
+    //    }
+    //
+    //    public void setId(Integer id) {
+    //        this.id = id;
+    //    }
     public Boolean getDeleted() {
         return deleted;
     }
@@ -74,18 +96,17 @@ public class Persistence extends BaseEntity {
         this.deleted = deleted;
     }
 
-//    public Date getCreated() {
-//        return created;
-//    }
-//
-//    public void setCreated(Date created) {
-//        this.created = created;
-//    }
-//
-//    public Date getChanged() {
-//        return changed;
-//    }
-
+    //    public Date getCreated() {
+    //        return created;
+    //    }
+    //
+    //    public void setCreated(Date created) {
+    //        this.created = created;
+    //    }
+    //
+    //    public Date getChanged() {
+    //        return changed;
+    //    }
     public void setChanged(Date changed) {
         this.changed = changed;
     }
@@ -98,31 +119,28 @@ public class Persistence extends BaseEntity {
         this.comment = comment;
     }
 
-//    @Override
-//    public int hashCode() {
-//        int hash = 0;
-//        hash += (id != null ? id.hashCode() : 0);
-//        return hash;
-//    }
-//
-//    @Override
-//    public boolean equals(Object object) {
-//        // TODO: Warning - this method won't work in the case the id fields are not set
-//        if (!(object instanceof Persistence)) {
-//            return false;
-//        }
-//        Persistence other = (Persistence) object;
-//        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-//            return false;
-//        }
-//        return true;
-//    }
-
+    //    @Override
+    //    public int hashCode() {
+    //        int hash = 0;
+    //        hash += (id != null ? id.hashCode() : 0);
+    //        return hash;
+    //    }
+    //
+    //    @Override
+    //    public boolean equals(Object object) {
+    //        // TODO: Warning - this method won't work in the case the id fields are not set
+    //        if (!(object instanceof Persistence)) {
+    //            return false;
+    //        }
+    //        Persistence other = (Persistence) object;
+    //        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+    //            return false;
+    //        }
+    //        return true;
+    //    }
     @Override
     public String toString() {
         return "org.dobi.entities.Persistence[ id=" + getId() + " ]";
     }
-    
-    
 
 }
